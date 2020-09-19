8b4bcde7d68a6330a7a7fffd8e189456bb56804d
Sang Le <sang.le@dodl.es>
2020-09-19 17:47:14 -0500

    Add styling for the widget

--- CustomJavascriptWidget/index.js
+++ CustomJavascriptWidget/index.js
@@ -3,28 +3,30 @@
 // The finished widget should display some basic information about the first 5 items in the feed such as title, url, and domain.
 // The widget should fit within a standard ad size of 300px wide by 600px tall.
 // Design is up to you.  Creativity is encouraged.
+
+//To change how many articles will be displayed. If increase the limit, reduce the -webkit-line-clamp in css file.
 const LIMIT_NUMBER = 5;
 
 $.getJSON('https://www.reddit.com/r/worldnews.json', function (dataset) {
 	var items = [];
 	let data = dataset.data.children;
-	console.log(data);
 
-	for (let i = 0; i < 5; i++) {
+	for (let i = 0; i < LIMIT_NUMBER; i++) {
 		let title = data[i].data.title;
 		let name = data[i].data.name;
 		let url = data[i].data.url;
 		//to not capitalize after . in domain
 		let [domain, path] = data[i].data.domain.split('.');
-		console.log(domain);
 
 		items.push(`
-		<div class="widget-element card">
+		<div class="widget-element-container card">
 			<a href=${url} target='_blank'>
-				<div id=${name} class='title'> ${title} </div>
-				<div class='domain-path'>
-					<div><span class='domain'>${domain}</span><span>.${path}</span></div>
-					<i class="fas fa-chevron-right"></i>
+				<div class="widget-elements">
+					<div id=${name} class='title line-clamp'> ${title} </div>
+					<div class='domain-path'>
+						<div><span class='domain'>${domain}</span><span>.${path}</span></div>
+						<i class="fas fa-chevron-right"></i>
+					</div>
 				</div>
 			</a>
 
@@ -36,3 +38,5 @@ $.getJSON('https://www.reddit.com/r/worldnews.json', function (dataset) {
 		html: items.join('')
 	}).appendTo('.widget');
 });
+
+// If I have more time, I planned to add pictures of artile in the box.
