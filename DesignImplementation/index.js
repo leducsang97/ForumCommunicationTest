2d20f0c7fbc7dd1b2065e720054aee20d90ef7ef
Sang Le <sang.le@dodl.es>
2020-09-20 18:36:16 -0500

    Design Implementation

---
+++ DesignImplementation/index.js
@@ -0,0 +1,5 @@
+// Design Implementation
+
+// Break the article design in the attached png file (HTML_CSS Quiz) into a functional web page using css grid for both mobile and desktop layouts.  Since this is a flat file and not a layered Photoshop/Sketch, document font choices, colors, etc can be chosen to match as closely as possible. Use any text you'd like, as long as it fills in about the same amount of text as the design.
+
+// Recommended Time Allowance:  2 hours or less
